439d1f745f2cf61fa8db085d1cffb340e9239860
Nitin <nitinkukreti12@gmail.com>
2021-05-24 18:47:28 +0530

    day one completed

--- app1/urls.py
+++ app1/urls.py
@@ -2,5 +2,7 @@
 from django.urls import path
 from .views import *
 urlpatterns=[
-   path('',homePage)
+   path('',homePage),
+   path('contact',contact),
+   path('about',about),
 ]
--- app1/views.py
+++ app1/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http.response import HttpResponse
 
 # Create your views here.
+def homePage(request):
+    # return render(request,template_name='hompage.html',context={'data':"nitin","id":1})
+    return render(request,'hompage.html',{'data':"nitin","id":1})
+
+def contact(request):
+    return HttpResponse("this is contact page")
+
+def about(request):
+    return render(request,'about.html',{'name':"nitin"})
